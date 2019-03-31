8e663de3434249d1518203d58ce05531a0aea1cb
Weiyun Lu <wlu058@uottawa.ca>
2019-03-31 17:13:21 -0400

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "Welcome to Radiant Realms.  Please hold!"
+  def home
+  end
+
+  def about
+  end
+
+  def gallery
+  end
+
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'home' => 'static_pages#home', as: :home
-  get 'about' => 'static_pages#about', as: :about
-  get 'gallery' => 'static_pages#gallery', as: :gallery
-  get 'contact' => 'static_pages#contact', as: :contact
+  get '/home', to: 'static_pages#home', as: :home
+  get '/about', to: 'static_pages#about', as: :about
+  get '/gallery', to: 'static_pages#gallery', as: :gallery
+  get '/contact', to: 'static_pages#contact', as: :contact
   Rails.application.routes.draw do
-    root :home
+    root 'static_pages#home'
   end
 end
